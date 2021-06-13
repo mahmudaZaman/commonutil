d1150e325640e978e675eec7d24d63ce2ef98e50
Md Najim Ahmed <najim.ju@gmail.com>
2021-06-13 15:16:23 +0800

    Update logger.go

--- comutil/logger.go
+++ comutil/logger.go
@@ -17,6 +17,7 @@ func NewLogger(traceID, spanID string) *zap.Logger {
 	config := zap.NewDevelopmentConfig()
 	config.OutputPaths = []string{
 		"stdout",
+		"/var/log/timelyship/account.log",
 	}
 	config.EncoderConfig.LevelKey = "level"
 	config.EncoderConfig.TimeKey = "timestamp"
